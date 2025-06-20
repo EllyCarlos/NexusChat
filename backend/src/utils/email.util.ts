bc8131dda679a7ba36fdaa2c2acc76473be4ea02
EllyCarlos <ellycarlos7915@gmail.com>
2025-06-21 00:24:14 +0300

    Update email.util.ts

--- backend/src/utils/email.util.ts
+++ backend/src/utils/email.util.ts
@@ -1,14 +1,46 @@
 import { transporter } from "../config/nodemailer.config.js"
-import { otpVerificationBody, privateKeyRecoveryBody,resetPasswordBody, welcomeEmailBody } from "../constants/emails/email.body.js"
+import { otpVerificationBody, privateKeyRecoveryBody, resetPasswordBody, welcomeEmailBody } from "../constants/emails/email.body.js"
 import { otpVerificationSubject, privateKeyRecoverySubject, resetPasswordSubject, welcomeEmailSubject } from "../constants/emails/email.subject.js"
 import type { EmailType } from "../interfaces/email/email.interface.js"
 import { env } from "../schemas/env.schema.js"
 
-export const sendMail = async(to:string,username:string,type:EmailType,resetUrl?:string,otp?:string,verificationUrl?:string)=>{
+export const sendMail = async(
+    to: string,
+    username: string,
+    type: EmailType,
+    resetUrl?: string,
+    otp?: string,
+    verificationUrl?: string
+) => {
+    let subject: string;
+    let htmlContent: string;
+
+    // Get subjects
+    switch (type) {
+        case 'OTP':
+            subject = otpVerificationSubject;
+            htmlContent = otpVerificationBody(username, otp!);
+            break;
+        case 'resetPassword':
+            subject = resetPasswordSubject;
+            htmlContent = resetPasswordBody(username, resetUrl!);
+            break;
+        case 'welcome':
+            subject = welcomeEmailSubject;
+            htmlContent = welcomeEmailBody(username);
+            break;
+        case 'privateKeyRecovery':
+            subject = privateKeyRecoverySubject;
+            htmlContent = privateKeyRecoveryBody(username, verificationUrl!);
+            break;
+        default:
+            throw new Error(`Unsupported email type: ${type}`);
+    }
+
     await transporter.sendMail({
-        from:env.EMAIL, 
+        from: env.EMAIL,
         to,
-        subject:type==='OTP'?otpVerificationSubject:type==='resetPassword'?resetPasswordSubject:type==='welcome'?welcomeEmailSubject:privateKeyRecoverySubject,
-        html:type==='OTP'?otpVerificationBody(username,otp!):type==='resetPassword'?resetPasswordBody(username,resetUrl!):type==='welcome'?welcomeEmailBody(username):privateKeyRecoveryBody(username,verificationUrl!),
-    })
+        subject,
+        html: htmlContent,
+    });
 }
