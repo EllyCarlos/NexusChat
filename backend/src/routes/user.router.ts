4423fbddfa634f7039f49c41fd2e3bd0958ff19b
EllyCarlos <ellycarlos7915@gmail.com>
2025-06-21 17:29:38 +0300

    Update user.router.ts

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { testEmailHandler, udpateUser } from "../controllers/user.controller.js";
+import { testEmailHandler, updateUser } from "../controllers/user.controller.js";
 import { fileValidation } from "../middlewares/file-validation.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/verify-token.middleware.js";
 
 export default Router()
 
-.patch("/",verifyToken,upload.single("avatar"),fileValidation,udpateUser)
+.patch("/",verifyToken,upload.single("avatar"),fileValidation,updateUser)
 .get("/test-email",verifyToken,testEmailHandler)
