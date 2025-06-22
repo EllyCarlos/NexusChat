7a1688f3ccbaf0da176bdcb390963c388e963217
EllyCarlos <ellycarlos7915@gmail.com>
2025-06-23 00:11:17 +0300

    callService origin

--- frontend/src/lib/server/services/callService.ts
+++ frontend/src/lib/server/services/callService.ts
@@ -1,46 +1,70 @@
-
+import { Prisma } from "@prisma/client";
 import { prisma } from "../prisma";
 
-// Prisma query function
-export const fetchUserCallHistory = async ({
-  loggedInUserId,
-}: {
-  loggedInUserId: string;
-}) => {
-  const callHistory = await prisma.callHistory.findMany({
-    where: {
-      OR: [
-        { callerId: loggedInUserId },
-        { calleeId: loggedInUserId }
-      ]
-    },
-    select: {
-      id: true,
-      startedAt: true,
-      endedAt: true,
-      status: true,
-      callee: {
-        select: {
-          id: true,
-          username: true,
-          avatar: true,
-          verificationBadge: true,
-        }
-      },
-      caller: {
-        select: {
-          id: true,
-          username: true,
-          avatar: true,
-          verificationBadge: true,
-        }
-      }
-    },
-    orderBy: {
-      startedAt: "desc"
-    },
-    take: 40
-  });
+// prisma query function
+export const fetchUserCallHistory = async({loggedInUserId}:{loggedInUserId:string})=>{
 
-  return callHistory;
-};
+    const callHistory = await prisma.callHistory.findMany({
+        where:{
+            OR:[
+                {callerId:loggedInUserId},
+                {calleeId:loggedInUserId}
+            ]
+        },
+        include:{
+            callee:{
+                select:{
+                    id:true,
+                    username:true,
+                    avatar:true,
+                    verificationBadge:true,
+                }
+            },
+            caller:{
+                select:{
+                    id:true,
+                    username:true,
+                    avatar:true,
+                    verificationBadge:true,
+                }
+            }
+        },
+        omit:{
+            callerId:true,
+            calleeId:true,
+        },
+        orderBy:{
+            startedAt:"desc"
+        },
+        take:40,
+    })
+
+    return callHistory;
+}
+
+
+// types
+export type fetchUserCallHistoryResponse = Prisma.CallHistoryGetPayload<{
+    include:{
+        callee:{
+            select:{
+                id:true,
+                username:true,
+                avatar:true,
+                verificationBadge:true,
+            }
+        },
+        caller:{
+            select:{
+                id:true,
+                username:true,
+                avatar:true,
+                verificationBadge:true,
+            }
+        }
+    },
+    omit:{
+        callerId:true,
+        calleeId:true,
+    },
+}>
