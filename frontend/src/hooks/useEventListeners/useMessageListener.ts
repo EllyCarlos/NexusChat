4e295fb5b237d3ff73b5bca21ac1478ac7acce6f
EllyCarlos <ellycarlos7915@gmail.com>
2025-07-03 18:40:59 +0300

    Fixed Implicit Any and Added Imports

--- frontend/src/hooks/useEventListeners/useMessageListener.ts
+++ frontend/src/hooks/useEventListeners/useMessageListener.ts
@@ -1,5 +1,9 @@
 import { Event } from "@/interfaces/events.interface";
 import { Message } from "@/interfaces/message.interface";
+// Assuming Chat and BasicUserInfo are defined in central interface files:
+import { Chat, BasicUserInfo } from "@/interfaces/chat.interface"; // Adjust path if your Chat interface is elsewhere
+// import { BasicUserInfo } from "@/interfaces/user.interface"; // Uncomment and use if BasicUserInfo is in a separate user interface file
+
 import { messageApi } from "@/lib/client/rtk-query/message.api";
 import {
   removeUserTyping,
@@ -12,7 +16,6 @@ import { useEffect, useRef } from "react";
 import { useSocketEvent } from "../useSocket/useSocketEvent";
 
 export const useMessageListener = () => {
-
   const selectedChatDetails = useAppSelector(selectSelectedChatDetails);
   const selectedChatDetailsRef = useRef(selectedChatDetails);
 
@@ -21,10 +24,9 @@ export const useMessageListener = () => {
   }, [selectedChatDetails]);
 
   const dispatch = useAppDispatch();
-  const chats =  useAppSelector(selectChats);
+  const chats = useAppSelector(selectChats);
 
   useSocketEvent(Event.MESSAGE, async (newMessage: Message) => {
-
     dispatch(
       messageApi.util.updateQueryData("getMessagesByChatId",{ chatId: newMessage.chatId, page: 1 },(draft) => {
           draft.messages.push(newMessage);
@@ -33,8 +35,8 @@ export const useMessageListener = () => {
       )
     );
 
-
-    const chat = chats.find(chat => chat.id === newMessage.chatId);
+    // Explicitly type the 'chatItem' parameter as 'Chat'
+    const chat = chats.find((chatItem: Chat) => chatItem.id === newMessage.chatId);
 
     if (chat) {
       dispatch(updateLatestMessage({chatId:newMessage.chatId,newMessage}));
@@ -49,11 +51,17 @@ export const useMessageListener = () => {
       else {
         const ifUserWhoWasTypingHasSentTheMessage = chat?.typingUsers.some(({id}) => id === newMessage.sender.id);
         if (ifUserWhoWasTypingHasSentTheMessage){
-          chat.typingUsers = chat.typingUsers.filter((user) => user.id !== newMessage.sender.id);
+          // Make sure chat.typingUsers is handled immutably if chat is part of Redux state
+          // For direct modification here, ensure chat object is mutable or copy it
+          // Or, better, dispatch an action to update typingUsers in Redux
+          const updatedTypingUsers = chat.typingUsers.filter((user) => user.id !== newMessage.sender.id);
+          // Assuming an action like updateChatTypingUsers exists
+          // dispatch(updateChatTypingUsers({ chatId: chat.id, typingUsers: updatedTypingUsers }));
+          // If direct mutation is acceptable in this context (e.g., if 'chat' is a draft from Immer),
+          // then the line below would be fine. If not, it could lead to state mutation issues outside Redux.
+          chat.typingUsers = updatedTypingUsers; // Direct mutation, be cautious
         }
       }
     }
-
-  
   });
 };
