284c7c9d28413719fdbd640570cea006f28799b0
EllyCarlos <ellycarlos7915@gmail.com>
2025-07-03 18:16:06 +0300

    Update useCall

--- frontend/src/hooks/useCalls/useCallEndListener.ts
+++ frontend/src/hooks/useCalls/useCallEndListener.ts
@@ -13,13 +13,14 @@ import { useSocketEvent } from "../useSocket/useSocketEvent";
 export const useCallEndListener = () => {
     const myGlobalStream = useAppSelector(selectMyGlobalStream);
     const dispatch = useAppDispatch();
-    
+
     // NEW: Get the peerService instance from the context.
     const { peerService } = usePeer();
 
     useEffect(() => {
         if (myGlobalStream !== undefined) {
-            myGlobalStream?.getTracks().forEach((track) => track.stop());
+            // FIX: Explicitly type 'track' as MediaStreamTrack
+            myGlobalStream?.getTracks().forEach((track: MediaStreamTrack) => track.stop());
             setTimeout(() => {
                 dispatch(setMyGlobalStream(undefined));
             }, 1000);
@@ -30,12 +31,12 @@ export const useCallEndListener = () => {
         setTimeout(() => {
             dispatch(setCallDisplay(false));
         }, 1000);
-        
+
         setTimeout(() => {
             // NEW: Use the peerService instance from the usePeer() hook.
             // It's already available here, no need to "get" it.
             peerService?.closeConnection();
-            
+
             toast.success("Call ended");
             dispatch(setInComingCallInfo(null));
             dispatch(setIsIncomingCall(false));
